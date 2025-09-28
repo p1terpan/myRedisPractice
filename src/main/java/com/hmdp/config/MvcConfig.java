b82048831ce5be260daa14654974208bf50322ae
16565 <peterpandd@qq.com>
2025-09-28 23:15:37 +0800

    first commit

---
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -0,0 +1,30 @@
+package com.hmdp.config;
+
+import com.hmdp.utils.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Jupiter
+ * @version 1.0
+ * @description 拦截器配置
+ * @date 2025/9/28 22:45
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .excludePathPatterns(
+                        "/user/code",
+                        "/user/login",
+                        "/blog/hot",
+                        "/shop/**",
+                        "/shop-type/**",
+                        "/voucher/**",
+                        "/uppload/**"
+                );
+    }
+}
---
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,45 @@
+package com.hmdp.utils;
+
+import com.hmdp.dto.UserDTO;
+import com.hmdp.entity.User;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author Jupiter
+ * @version 1.0
+ * @description 全局拦截器
+ * @date 2025/9/28 22:39
+ */
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 1.获取session
+        HttpSession session = request.getSession();
+        // 2.获取session中的用户
+        Object user = session.getAttribute("user");
+        // 3.判断用户是否存在
+        if (user == null) {
+            // 4.不存在拦截
+            response.setStatus(401); // 返回未授权
+            return false;
+        }
+        // 5.存在保存到threadlocal，这里用tl是因为需要有一个东西把user穿过拦截器带出去
+        UserHolder.saveUser((UserDTO) user);
+        return true;
+    }
+
+//    @Override
+//    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+//        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+//    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        UserHolder.removeUser();
+    }
+}
